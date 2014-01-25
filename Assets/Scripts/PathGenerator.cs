286cd164d5c6ab1030bb321e29225a13c6e9d34c
juwambo <juwambo@gmail.com>
2014-01-25 02:03:11 +0100

    Added PathGenerator & Spline Controller

---
+++ Assets/Scripts/PathGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SplineController))]
+public class PathGenerator : MonoBehaviour {
+	public float maxSectionAngle = 30.0f;
+	public float sectionDistance = 10.0f;
+	public float sectionOffset = 2.0f;
+	
+	public Transform splinePath;
+	
+	private SplineController splineController;
+	
+	// Use this for initialization
+	void Start () {
+		splineController = this.GetComponent<SplineController>();
+		for(int i = 0; i < 10; i++)
+		{
+			NextNode();	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space))
+		{
+			splineController.FollowSpline();	
+		}
+	}
+	
+	void NextNode()
+	{
+		Transform last = this.transform;
+		
+		Transform newNode = new GameObject("" + splinePath.childCount).transform;
+		
+		newNode.transform.position = last.position;
+		
+		if(splinePath.childCount > 0)
+		{
+			last = splinePath.GetChild(splinePath.childCount - 1);
+			newNode.position = last.position + last.forward * sectionDistance + last.right * Random.Range(-sectionOffset, sectionOffset) + last.up * Random.Range(-sectionOffset, sectionOffset);
+		}
+		
+		newNode.parent = splinePath;
+	}
+}
